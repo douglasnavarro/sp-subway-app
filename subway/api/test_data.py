ef2cb85f45fb9f870656d4d067bf4e02b4e12fd2
Douglas Navarro <douglasnavarro94@gmail.com>
2018-09-24 19:47:32 -0300

    added test cases for list of readings

---
+++ subway/api/test_data.py
@@ -0,0 +1,27 @@
+test_data = [
+    {
+        'timestamp':'2018-09-24T17:25',
+        'line':'vermelha',
+        'status':'velocidade reduzida'
+    },
+    {
+        'timestamp':'2018-09-24T17:25',
+        'line':'azul',
+        'status':'normal'
+    },
+    {
+        'timestamp':'2018-09-24T17:25',
+        'line':'amarela',
+        'status':'paralisada'
+    },
+    {
+        'timestamp':'2018-09-22T12:00',
+        'line':'verde',
+        'status':'normal'
+    },
+    {
+        'timestamp':'2018-09-12T10:00',
+        'line':'jade',
+        'status':'normal'
+    },
+]
--- subway/api/tests.py
+++ subway/api/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Reading
+from .serializers import ReadingSerializer
+from django.urls import reverse
+from .test_data import test_data
 
-# Create your tests here.
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_reading(timestamp, status, line):
+        if 'timestamp' != "" and 'line' != "" and status != "":
+            Reading.objects.create(timestamp=timestamp, line=line, status=status)
+    
+    def setUp(self):
+        for reading in test_data:
+            self.create_reading(**reading)
+    
+class GetAllReadingsTest(BaseViewTest):
+
+    def test_get_all_readings(self):
+        response = self.client.get('/api/')
+        expected = Reading.objects.all()
+        serialized = ReadingSerializer(expected, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serialized.data)
+        
+    def test_line_filter(self):
+        response = self.client.get('/api/?line=vermelha')
+        expected = Reading.objects.filter(line='vermelha')
+        serialized = ReadingSerializer(expected, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serialized.data)
+    
+    def test_timestamp_filter(self):
+        response = self.client.get('/api/')
