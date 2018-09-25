45797519e69d8eb89e35c4fcee7d23495ffada30
Douglas Navarro <douglasnavarro94@gmail.com>
2018-09-25 11:36:27 -0300

    implemented tests for post requests

--- subway/api/tests.py
+++ subway/api/tests.py
@@ -1,9 +1,12 @@
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
 from .models import Reading
+from django.contrib.auth.models import User, Permission
 from .serializers import ReadingSerializer
 from django.urls import reverse
 from .test_data import test_data
+from datetime import datetime
+
 
 class BaseViewTest(APITestCase):
     client = APIClient()
@@ -36,4 +39,34 @@ class GetAllReadingsTest(BaseViewTest):
         self.assertEqual(response.data, serialized.data)
     
     def test_timestamp_filter(self):
-        response = self.client.get('/api/')
+        response = self.client.get('/api/?timestamp_after=2018-09-11&timestamp_before=2018-09-21')
+        expected = Reading.objects.filter(timestamp__range=(datetime(2018,9,11), datetime(2018,9,21)))
+        serialized = ReadingSerializer(expected, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serialized.data)
+
+class PostReadingsTest(BaseViewTest):
+
+    def setUp(self):
+
+        user = User.objects.create(username='test_user')
+        user.set_password('test_user123')
+        permission = Permission.objects.get(name='Can add reading')
+        user.user_permissions.add(permission)
+        user.save()
+
+    def test_post_forbidden(self):
+        payload = {'timestamp':'2018-09-12T10:00', 'line':'jade', 'status':'normal'}
+        response = self.client.post('/api/', payload)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_post_allowed(self):
+        self.client.login(username='test_user', password='test_user123')
+        payload = {'timestamp':'2018-09-25T13:00', 'line':'safira', 'status':'paralisada'}
+        response = self.client.post('/api/', payload, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.client.logout()
