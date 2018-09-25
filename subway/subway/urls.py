40f0e88f7cfe444e206c06e2495cd2c72f52cf89
Douglas Navarro <douglasnavarro94@gmail.com>
2018-09-25 19:38:48 -0300

    initialized dashboard app with dummy index

--- subway/subway/urls.py
+++ subway/subway/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('dashboard.urls')),
 ]
