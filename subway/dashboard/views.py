535b15116d3f2e01637e995229fd32fede6562b3
Douglas Navarro <douglasnavarro94@gmail.com>
2018-09-26 10:18:29 -0300

    added dummy view with readings data

--- subway/dashboard/views.py
+++ subway/dashboard/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
-
+from api.models import Reading
+from api.serializers import ReadingSerializer
+import json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    readings = Reading.objects.all()
+    serializer = ReadingSerializer(readings, many=True)
+    return HttpResponse(json.dumps(serializer.data))
